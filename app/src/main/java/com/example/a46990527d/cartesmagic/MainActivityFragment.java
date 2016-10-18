25acc331fb1e17a0a317ec1d0cdf6bf523efac37
aacerete <46990527d@iespoblenou>
2016-10-18 16:35:19 +0200

    Creem l' ArrayList<String> items i l'ArrayAdapter<String> adapter que enllaçara amb el listView

--- app/src/main/java/com/example/a46990527d/cartesmagic/MainActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/cartesmagic/MainActivityFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -21,6 +27,18 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvCartes = (ListView) view.findViewById(R.id.lvCartes);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_cartes_row,
+                R.id.tvCartes,
+                items
+        );
+
+        lvCartes.setAdapter(adapter);
+
+
 
         return view;
     }
