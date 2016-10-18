953ad886e773e0cf7573d7252151d75899998553
aacerete <46990527d@iespoblenou>
2016-10-18 17:43:42 +0200

    amb el metode onOptionsItemSelected, fem que quan clickem al menuitem refresh es cridi a un metode anomenat refresh() que deixem preparat

--- app/src/main/java/com/example/a46990527d/cartesmagic/MainActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/cartesmagic/MainActivityFragment.java
@@ -4,6 +4,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -30,6 +33,7 @@ public class MainActivityFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -54,4 +58,34 @@ public class MainActivityFragment extends Fragment {
 
         return view;
     }
+
+    //agreguem el menuItem refresh al Menu
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_cartes_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int idmenuitem = item.getItemId();
+
+        if (idmenuitem == R.id.action_Refresh) {
+            refresh();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+
+    }
+
+
+
+
 }
