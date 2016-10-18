1e7f80bcfabaad059c0aba51508a2e70abe29d29
aacerete <46990527d@iespoblenou>
2016-10-18 17:52:41 +0200

    Creem la classe Card, que contindra la informació del JSON que ens proporciona la API. En principi només buscarem el nom i el tipus. Generem els getters setters, i el toString

---
+++ app/src/main/java/com/example/a46990527d/cartesmagic/Card.java
@@ -0,0 +1,38 @@
+package com.example.a46990527d.cartesmagic;
+
+/**
+ * Created by 46990527d on 18/10/16.
+ */
+
+public class Card {
+
+    private String name;
+    private String type;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+
+}
